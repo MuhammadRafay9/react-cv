ff20b5b2bc3a52183978627c8174584d11165972
Hiten Kumar <hitenkumarvendani@gmail.com>
2022-09-09 21:24:17 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,43 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Muhammad Rafay</h1>
+<hr />
+<h3>Objective</h3>
+<p>A result-oriented individual with BS Software Engineering, seeking a suitable position at a reputed
+organization where I can use my extensive knowledge in software design and development and further
+learn new things and grow as a Software Engineer.</p>
+<hr />
+
+<h3>Education</h3>
+<p>
+BS Computer Science*
+Virtual University,Pakistan
+2021 - Present,
+</p>
+<hr />
+<h3>Skills</h3>
+<p>
+	Html
+	<br />
+	Word
+	<br />
+	Excel
+
+</p>
+
+<hr />
+
+<h3>Personal Information</h3>
+
+<p>
+	Email - mrafay08@gmail.com
+	<br />
+	Phone - 03422136104
+</p>
+
+
+
     </div>
   );
 }
